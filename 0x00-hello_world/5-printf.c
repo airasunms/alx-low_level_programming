0dddce28b20ad087337510561fe8269dfbc12b5b
Wadud Saani <muslimsaani@gmail.com>
2023-02-17 07:50:32 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this prints the wording in the function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+
+}	
