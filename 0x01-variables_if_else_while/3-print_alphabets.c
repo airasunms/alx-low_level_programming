24664300f645d21aa691a5422c0c7f06a01fc7cb
Wadud Saani <muslimsaani@gmail.com>
2023-02-19 18:58:49 -0800

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters in lowercase and then uppercase
+ *
+ * Return: when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
